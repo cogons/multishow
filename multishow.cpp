7fa48d25ca3d53fdf1641b8e60d69ccd9cf00490
cogons <2012029080015@std.uestc.edu.cn>
2016-05-11 16:38:07 +0800

    Create multishow.cpp

---
+++ multishow.cpp
@@ -0,0 +1,95 @@
+
+
+
+void multishow(const std::string& _winName, const vector<Mat>& _img)
+{
+	int nImg = (int)_img.size();
+	
+	vector<cv::Mat> _img3;
+
+	for (int i = 0; i < nImg; i++){
+		Mat _img3Mat;
+		if (_img[i].channels() == 1){
+			cvtColor(_img[i], _img3Mat, CV_GRAY2BGR);
+			_img3.push_back(_img3Mat);
+		}
+	}
+
+	Mat dispImg;
+
+	int size;
+	int x, y;
+
+	// w - Maximum number of images in a row 
+	// h - Maximum number of images in a column 
+	int w, h;
+	// scale - How much we have to resize the image
+	float scale;
+	int max;
+
+	if (nImg <= 0)
+	{
+		printf("Number of arguments too small....\n");
+		return;
+	}
+	else if (nImg > 12)
+	{
+		printf("Number of arguments too large....\n");
+		return;
+	}
+
+	else if (nImg == 1)
+	{
+		w = h = 1;
+		size = 300;
+	}
+	else if (nImg == 2)
+	{
+		w = 2; h = 1;
+		size = 300;
+	}
+	else if (nImg == 3 || nImg == 4)
+	{
+		w = 2; h = 2;
+		size = 300;
+	}
+	else if (nImg == 5 || nImg == 6)
+	{
+		w = 3; h = 2;
+		size = 200;
+	}
+	else if (nImg == 7 || nImg == 8)
+	{
+		w = 4; h = 2;
+		size = 200;
+	}
+	else
+	{
+		w = 4; h = 3;
+		size = 150;
+	}
+
+	dispImg.create(Size(100 + size*w, 60 + size*h), CV_8UC3);
+
+	for (int i = 0, m = 20, n = 20; i<nImg; i++, m += (20 + size))
+	{
+		x = _img3[i].cols;
+		y = _img3[i].rows;
+
+		max = (x > y) ? x : y;
+		scale = (float)((float)max / size);
+
+		if (i%w == 0 && m != 20)
+		{
+			m = 20;
+			n += 20 + size;
+		}
+
+		Mat imgROI = dispImg(Rect(m, n, (int)(x / scale), (int)(y / scale)));
+		
+		resize(_img3[i], imgROI, Size((int)(x / scale), (int)(y / scale)));
+	}
+
+	namedWindow(_winName);
+	imshow(_winName, dispImg);
+}
